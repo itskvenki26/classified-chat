3163d3dab4f31cfe434d5509d1a31d7a5c0b5da7
itskvenki26 <itskvenki26@gmail.com>
2014-05-10 23:40:42 -0400

    test commit

--- src/Models/CurrentUser.java
+++ src/Models/CurrentUser.java
@@ -5,11 +5,11 @@ import org.json.JSONObject;
 
 public class CurrentUser {
 
-	private static int id;
-	private static long crid;
-	private static String Cur_Mak;
-	private static String Cur_Ekey;
-	private static String Old_Mak;
+	private int id;
+	private long crid;
+	private String Cur_Mak;
+	private String Cur_Ekey;
+	private String Old_Mak;
 	
 	public static final String CU_CRID="current user crid";
 	public static final String CU_CUR_MAK = "current user current MAK";
@@ -21,21 +21,21 @@ public class CurrentUser {
      }
      // constructor
      public CurrentUser(int id,long crid, String Cur_Mak, String Cur_Ekey, String Old_Mak/*,ArrayList<Channel> Chlist*/){
-         CurrentUser.id      = id;
-         CurrentUser.crid=crid;
-         CurrentUser.Cur_Mak    = Cur_Mak;
-         CurrentUser.Cur_Ekey    = Cur_Ekey;
-         CurrentUser.Old_Mak = Old_Mak;
+         this.id      = id;
+         this.crid=crid;
+         this.Cur_Mak    = Cur_Mak;
+         this.Cur_Ekey    = Cur_Ekey;
+         this.Old_Mak = Old_Mak;
         // this.Chlist=Chlist;
           
      }
 
-	public static int getId() {
-		return id;
+	public int getId() {
+		return this.id;
 	}
 
-	public static void setId(int id) {
-		CurrentUser.id = id;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getCur_Mak() {
@@ -45,28 +45,28 @@ public class CurrentUser {
 		return crid;
 	}
 
-	public static void setcrid(long crid) {
-		CurrentUser.crid = crid;
+	public  void setcrid(long crid) {
+		this.crid = crid;
 	}
 
-	public static void setCur_Mak(String Cur_Mak) {
-		CurrentUser.Cur_Mak = Cur_Mak;
+	public void setCur_Mak(String Cur_Mak) {
+		this.Cur_Mak = Cur_Mak;
 	}
 
 	public String getCur_Ekey() {
 		return Cur_Ekey;
 	}
 
-	public static void setCur_Ekey(String Cur_Ekey) {
-		CurrentUser.Cur_Ekey = Cur_Ekey;
+	public void setCur_Ekey(String Cur_Ekey) {
+		this.Cur_Ekey = Cur_Ekey;
 	}
 
 	public String getOld_Mak() {
 		return Old_Mak;
 	}
 
-	public static void setOld_Mak(String Old_Mak) {
-		CurrentUser.Old_Mak = Old_Mak;
+	public void setOld_Mak(String Old_Mak) {
+		this.Old_Mak = Old_Mak;
 	}
 	
 	public JSONObject toJSON() throws JSONException {
